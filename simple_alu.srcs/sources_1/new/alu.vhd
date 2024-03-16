9288a27be9b72707907a70fbaf8d181f7434c70d
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-03-16 20:17:39 +0000

    add flags for hc, acr & avr

--- simple_alu.srcs/sources_1/new/alu.vhd
+++ simple_alu.srcs/sources_1/new/alu.vhd
@@ -34,19 +34,31 @@ use IEEE.NUMERIC_STD.ALL;
 entity alu is
     Port ( a : in STD_LOGIC_VECTOR (7 downto 0);
            b : in STD_LOGIC_VECTOR (7 downto 0);
-           btnU: in STD_LOGIC; 
-           btnL: in STD_LOGIC; 
-           btnR: in STD_LOGIC; 
-           btnD: in STD_LOGIC; 
-           led : out STD_LOGIC_VECTOR (7 downto 0));
+           btnU: in STD_LOGIC; -- Sum
+           btnL: in STD_LOGIC; -- And 
+           btnR: in STD_LOGIC; -- Or 
+           btnD: in STD_LOGIC; -- Xor 
+           led : out STD_LOGIC_VECTOR (7 downto 0);
+           acr: out STD_LOGIC; -- Carry flag
+           hc: out STD_LOGIC; -- Half-Carry flag
+           avr: out STD_LOGIC); -- Overflow flag
 end alu;
 
 architecture Behavioral of alu is
-
+    signal half_sum_low: STD_LOGIC_VECTOR (4 downto 0) := (others => '0');
+    signal half_sum_high: STD_LOGIC_VECTOR (4 downto 0) := (others => '0');
 begin
-    led <= std_logic_vector(unsigned(a) + unsigned(b)) when btnU = '1' else 
-           std_logic_vector(unsigned(a) and unsigned(b)) when btnL = '1' else
-           std_logic_vector(unsigned(a) or unsigned(b)) when btnR = '1' else
-           std_logic_vector(unsigned(a) xor unsigned(b)) when btnD = '1' else
+    -- Extend a from 4 to 5 bits to match half_sum_low size
+    half_sum_low <= std_logic_vector('0'&unsigned(a(3 downto 0)) + unsigned(b(3 downto 0)));
+    half_sum_high <= std_logic_vector('0'&unsigned(a(7 downto 4)) + unsigned(b(7 downto 4)) + unsigned'('0' & half_sum_low(4)));
+    
+    led <= (half_sum_high(3 downto 0) & half_sum_low(3 downto 0)) when btnU = '1' else 
+           std_logic_vector(a and b) when btnL = '1' else
+           std_logic_vector(a or b) when btnR = '1' else
+           std_logic_vector(a xor b) when btnD = '1' else
            (others => 'L');
+    
+    hc <= half_sum_low(4);
+    avr <= (a(7) and b(7) and not half_sum_high(3)) or (not a(7) and not b(7) and half_sum_high(3));
+    acr <= half_sum_high(4);
 end Behavioral;
